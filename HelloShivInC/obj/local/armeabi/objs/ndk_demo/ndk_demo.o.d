e43b9cd51cc71b500027051c9b3224f286247171
Shivanand <shivu.darur@gmail.com>
2015-04-08 19:46:52 +0530

    Initial commit

--- HelloShivInC/obj/local/armeabi/objs/ndk_demo/ndk_demo.o.d
+++
@@ -1,7 +0,0 @@
-obj/local/armeabi/objs/ndk_demo/ndk_demo.o: jni/ndk_demo.c \
- jni/com_c_helloshivinc_MainActivity.h \
- D:/Android_Downloads/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/jni.h
-
-jni/com_c_helloshivinc_MainActivity.h:
-
-D:/Android_Downloads/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/jni.h:
